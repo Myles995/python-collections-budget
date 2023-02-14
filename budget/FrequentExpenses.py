93971058949399792d7ce9a2fb86eaaf9c34304a
Myles McDermott <cmt_myles@hotmail.com>
2023-02-14 12:35:07 +1000

    1

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,15 @@
+from . import Expense
+import collections
+
+expenses = Expense.Expenses()
+
+expenses.read_expenses('data/spending_data.csv')
+
+spending_catagories = []
+
+for expense in expenses.list:
+    spending_catagories.append(expense.category)
+
+spending_counter = collections.Counter(spending_catagories)
+
+print(spending_counter)
