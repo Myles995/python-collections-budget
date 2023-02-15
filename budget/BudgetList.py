22770dd4ff6a12abe9f93813b1be76d8810dedde
Myles McDermott <cmt_myles@hotmail.com>
2023-02-16 09:57:59 +1000

    3

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -19,6 +19,19 @@ class BudgetList:
     def __len__(self):
         return self.expenses + self.overages 
 
+    def __iter__(self):
+        iter(self.expenses) 
+        self.iter_0
+        iter(self.overages)   
+        return self
+
+    def __next__(self):
+        try:
+            return __next__(self.iter_e)    
+        except StopIteration as stop:
+            return __next(self.iter_o)
+
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
@@ -31,34 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# class BudgetList:
-#     def __init__(self, budget):
-#         self.budget = budget
-#         self.sum_expenses = 0
-#         self.expenses = []
-#         self.sum_overages = 0
-#         self.overages = []
-
-#     def append(self, item):
-#         if self.sum_expenses + item < self.budget:
-#             self.expenses.append(item)  
-#             self.sum_expenses += item  
-#         else:
-#             self.overages.append(item)  
-#             self.sum_overages += item
-
-#     def __len__(self):
-#         return self.expenses.len() + self.overages.len()         
-
-# def main():
-#     myBudgetList = BudgetList(1200)
-#     expenses = Expense.Expenses()
-#     expenses.read_expenses("data/spending_data.csv")   
-
-#     for expense in expenses.list:
-#         myBudgetList.append(expense.amount)
-
-#     print("The count of all expenses: " + str(len(myBudgetList)))
-
-# if __name__ == "__main__":
-#     main()
